0da2895573b640bc21595f33e26e7ce0e5cd2994
Denis Palnitsky <palnitsky@gmail.com>
2025-03-30 10:45:29 +0200

    remove unused code

--- api/pkg/server/api.go
+++ api/pkg/server/api.go
@@ -194,24 +194,3 @@ type GetLocationResponse struct {
 }
 
 // end Locations
-
-// Leftovers
-type GetChallengesRequest struct {
-	Id string `path:"id" validate:"required"`
-}
-
-type ListChallengesResponse struct {
-	Challenges map[string]string `json:"challenges"`
-}
-
-type GetChallengesResponse struct {
-	Result string
-}
-
-type ThingPutRequest struct {
-	Name  string `path:"name" validate:"required"`
-	Value string `json:"value" validate:"required"`
-}
-
-type ThingPutResponse struct {
-}
