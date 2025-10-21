d522f3b236f32155028e6e942c2d9eafc7afe7a4
Denis Palnitsky <palnitsky@gmail.com>
2025-10-21 19:36:53 +0200

    Enhance Vite configuration to include admin proxy settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,6 +13,11 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
+      },
+      '/__admin': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
       }
     }
   }
